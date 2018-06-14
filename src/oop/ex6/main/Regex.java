4d9b94b337677c4526eeeff143ec56f743d94e3a
tomerpeled1 <tomer.peled1@mail.huji.ac.il>
2018-06-14 14:07:31 +0300

    more work

--- src/oop/ex6/main/Regex.java
+++ src/oop/ex6/main/Regex.java
@@ -1,4 +1,11 @@
 package oop.ex6.main;
 
+import java.util.regex.Pattern;
+
 public class Regex {
+
+	private static String type = "[(int)(double)(String)(boolean)(char)]";
+
+	static final Pattern FUNCTION_TEMPLATE = Pattern.compile("void" +". " type + "\\(");
+
 }
--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -9,8 +9,7 @@ import java.util.ArrayList;
 public class Sjavac {
 
 
-
-	private static String[] getLines(String path){
+	private static String[] getLines(String path) {
 		return null;
 	}
 
@@ -28,17 +27,25 @@ public class Sjavac {
 	}
 
 	private static void getGlobalDataMembers(String[] lines, ArrayList<FunctionWrapper> funcs, ArrayList<VariableWrapper> globalVars) {
-		for (String line:lines){
-			checkGlobalLine(funcs,globalVars,line);
+		int openBraces = 0;
+		for (String line : lines) {
+			if (openBraces == 0) {
+				checkGlobalLine(funcs,globalVars,line);
+			}
+			if (line.endsWith("{")){
+				openBraces += 1;
+			} else if (line.endsWith("}")){
+				openBraces -= 1;
+			}
 		}
 	}
 
 	private static void checkGlobalLine(ArrayList<FunctionWrapper> funcs,
-	                             ArrayList<VariableWrapper> vars, String line){
+	                                    ArrayList<VariableWrapper> vars, String line) {
 
-		if (line.startsWith("void") ){
+		if (line.startsWith("void")) {
 			funcs.add(lineToFuncObj(line));
-		} else if (/*line starts with var decleration*/ ){
+		} else if (/*line starts with var decleration*/) {
 			vars.add(lineToVarObj(line));
 		}
 	}
@@ -48,5 +55,5 @@ public class Sjavac {
 	}
 
 	private static FunctionWrapper lineToFuncObj(String line) {
-
+		line.startsWith()
 	}

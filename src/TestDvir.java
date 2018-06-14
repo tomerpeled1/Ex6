7f9c7963197f729014f42d28fb5d952b07dcb583
DvirAmar <40214321+DvirAmar@users.noreply.github.com>
2018-06-14 17:59:37 +0300

    another sync

--- src/TestDvir.java
+++ src/TestDvir.java
@@ -1,4 +1,9 @@
 public class TestDvir {
     public static void main(String[] args) {
-        String expression;
+        String expression = "a || f && 3";
+        String s = "(3 || 5)";
+        int expressionStart = s.indexOf('(');
+        int expressionEnd = s.indexOf(')');
+
+    }
 }

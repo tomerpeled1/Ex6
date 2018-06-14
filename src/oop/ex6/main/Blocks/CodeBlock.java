f6445858f7638bca03ba8544fb6fcfad5b558052
DvirAmar <40214321+DvirAmar@users.noreply.github.com>
2018-06-14 17:28:42 +0300

    another sync

--- src/oop/ex6/main/Blocks/CodeBlock.java
+++ src/oop/ex6/main/Blocks/CodeBlock.java
@@ -3,8 +3,10 @@ package oop.ex6.main.Blocks;
 import com.sun.org.apache.bcel.internal.classfile.Code;
 import oop.ex6.VariableWrapper;
 import oop.ex6.main.IllegalLineException;
+import oop.ex6.main.Regex;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class CodeBlock {
@@ -27,7 +29,9 @@ public abstract class CodeBlock {
     }
 
     public void run() throws IllegalLineException {
+        while (
         String line = runner.GetNextLine();
+        CodeBlock nextBlock = null;
         //TODO check if variable declaration (aka int x = 3), check that it's not in this scope variables(can be in others)
 
         //TODO check if variable assigment (aka x = 3)
@@ -35,4 +39,10 @@ public abstract class CodeBlock {
         //TODO check a call to a fucntion, check if function is in the fnctions list and check paramenters
 
         //TODO check if if/while and a boolean expression.
+        Matcher ifMatcher = Regex.IF_CHECK.matcher(line);
+        Matcher whileMatcher = Regex.WHILE_CHECK.matcher(line);
+        String booleanExpression = null;
+        if (ifMatcher.matches()) {
+            booleanExpression =
+
 }
--- src/oop/ex6/main/VariableWrapper.java
+++ src/oop/ex6/main/VariableWrapper.java
@@ -6,11 +6,53 @@ public class VariableWrapper {
 	}
 
 	private String name;
-	private Boolean hasValue;
+	private boolean hasValue;
 	private Types type;
 
-	VariableWrapper (String line, Types type) {
-		//TODO - implement
+	/**
+	 * Constructs a VariableWrapper object with a type, hasValue and name.
+	 * @param type The type of the variable.
+	 * @param hasValue A boolean indicates if the variable has a value or not.
+	 * @param name The name of the variable.
+	 */
+	public VariableWrapper (String type, boolean hasValue, String name) {
+		this.hasValue = hasValue;
+		this.type = stringToTypes(type);
+		this.name = name;
 	}
+
+	/**
+	 * A getter for the hasValue property.
+	 * @return value of hasValue property.
+	 */
+	public boolean getHasValue() {
+		return this.hasValue;
+	}
+
+	/**
+	 * Puts a new value in hasValue.
+	 * @param hasValue Indicates if this variable is intiliazed or not.
+	 */
+	public void setHasValue(boolean hasValue) {
+		this.hasValue = hasValue;
+	}
+
+	private static Types stringToTypes(String str) {
+		switch (str) {
+			case "int":
+				return Types.INT;
+			case "double":
+				return Types.DOUBLE;
+			case "char":
+				return Types.CHAR;
+			case "boolean":
+				return Types.BOOLEAN;
+			case "String":
+				return Types.STRING;
+			default:
+				return null;
+		}
+	}
+
 
 }

1c23e3786bd03675bb2c75aeab60aa0085540df6
tomerpeled1 <tomer.peled1@mail.huji.ac.il>
2018-06-14 14:58:33 +0300

    started regexes

--- src/oop/ex6/main/Regex.java
+++ src/oop/ex6/main/Regex.java
@@ -6,6 +6,9 @@ public class Regex {
 
 	private static String type = "[(int)(double)(String)(boolean)(char)]";
 
-	static final Pattern FUNCTION_TEMPLATE = Pattern.compile("void" +". " type + "\\(");
+	private static String methodName = "[a-zA-Z]";
 
+	static final Pattern FUNCTION_TEMPLATE = Pattern.compile("void" +methodName+ "\\(");
+
+	void[ ]+[a-zA-z]\w*\([ ]*((double|String|boolean|char)[ ]+[a-zA-z]\w*[ ]*)+\){
 }

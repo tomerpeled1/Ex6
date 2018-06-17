e24b15be8066fac2c9fd0b2130468fba4514e39f
tomerpeled1 <tomer.peled1@mail.huji.ac.il>
2018-06-18 01:55:32 +0300

    started the check of function

--- src/oop/ex6/main/Blocks/CodeBlock.java
+++ src/oop/ex6/main/Blocks/CodeBlock.java
@@ -1,6 +1,7 @@
 package oop.ex6.main.Blocks;
 
 import com.sun.org.apache.bcel.internal.classfile.Code;
+import oop.ex6.main.FunctionWrapper;
 import oop.ex6.main.VariableWrapper;
 import oop.ex6.main.IllegalLineException;
 import oop.ex6.main.Regex;
@@ -10,116 +11,147 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public abstract class CodeBlock {
-    protected ArrayList<VariableWrapper> variables;
-    private CodeBlock parent;
-    protected static LinesRunner runner;
+	protected ArrayList<VariableWrapper> variables;
+	private CodeBlock parent;
+	protected static LinesRunner runner;
+	private MasterBlock master;
 
 
-    protected CodeBlock() {
-    }
+	protected CodeBlock() {
+	}
 
-    /**
-     * Constructs a new CodeBlock with a parent block.
-     * @param parent The block this block is contained in.
-     */
-    public CodeBlock(CodeBlock parent) {
-        this.parent = parent;
-    }
+	/**
+	 * Constructs a new CodeBlock with a parent block.
+	 *
+	 * @param parent The block this block is contained in.
+	 */
+	public CodeBlock(CodeBlock parent) {
+		this.parent = parent;
+		if (this instanceof MasterBlock) master = (MasterBlock) parent;
+		else master = parent.getMaster();
 
-    public void run() throws IllegalLineException {
-        String line = runner.GetNextLine();
-        Matcher BLOCK_END_MATCHER = Regex.BLOCK_END_PATTERN.matcher(line);
-        while (!BLOCK_END_MATCHER.matches()) {
-            CodeBlock nextBlock = null;
+	}
 
-            //TODO check if variable declaration (aka int x = 3), check that it's not in this scope variables(can be in others)
+	/*
+	returns the master of the program.
+	 */
+	private MasterBlock getMaster() {
+		return master;
+	}
 
-            //TODO check if variable assigment (aka x = 3)
+	public void run() throws IllegalLineException {
+		String line = runner.GetNextLine();
+		Matcher BLOCK_END_MATCHER = Regex.BLOCK_END_PATTERN.matcher(line);
+		while (!BLOCK_END_MATCHER.matches()) {
+			CodeBlock nextBlock = null;
 
-            //TODO check a call to a fucntion, check if function is in the fnctions list and check paramenters
+			//TODO check if variable declaration (aka int x = 3), check that it's not in this scope variables(can be in others)
 
-            //TODO check if a declaration of functoin only in master block and than run on it.
-            if (checkIfValidBooleanExpression(line)) {
-                nextBlock = new BooleanExpressionBlock(this);
-            }
-            if (nextBlock != null) {
-                nextBlock.run();
-            }
-        }
-    }
+			//TODO check if variable assigment (aka x = 3)
 
-    protected boolean InitliazeVariables(String line) {
+			//TODO check a call to a fucntion, check if function is in the fnctions list and check paramenters
+
+			//TODO check if a declaration of functoin only in master block and than run on it.
+			if (checkIfValidBooleanExpression(line)) {
+				nextBlock = new BooleanExpressionBlock(this);
+			}
+			if (nextBlock != null) {
+				nextBlock.run();
+			}
+		}
+	}
 
 
+	/**
+	 * Checks if a line opens a new if or while block.
+	 *
+	 * @param line The line to check if a opens a block.
+	 * @return True if a valid boolean expression, otherwise false.
+	 */
+	private boolean checkIfValidBooleanExpression(String line) throws IllegalLineException {
+		Matcher ifMatcher = Regex.IF_PATTERN.matcher(line);
+		Matcher whileMatcher = Regex.WHILE_PATTERN.matcher(line);
+		String booleanExpression = null;
+		int expressionStart = 0;
+		int expressionEnd = 0;
+		if (ifMatcher.matches() || whileMatcher.matches()) {
+			expressionStart = line.indexOf('(');
+			expressionEnd = line.indexOf(')');
+			booleanExpression = line.substring(expressionStart, expressionEnd);
+			String[] splitExpression = booleanExpression.split(Regex.BOOLEAN_EXPRESSION_SPLIT);
+			for (String exp : splitExpression) {
+				Matcher doubleMatch = Regex.DOUBLE_PATTERN.matcher(exp);
+				if (!(exp.equals("true") || exp.equals("false") || doubleMatch.matches() ||
+						checkIfInitializedVariable(exp))) {
+					throw new IllegalLineException("error in line " + runner.GetLineNumber() + ", boolean " +
+							"expression not supported.");
+				}
+			}
+			return true;
+		}
+		return false;
+	}
 
-    /**
-     * Checks if a line opens a new if or while block.
-     * @param line The line to check if a opens a block.
-     * @return True if a valid boolean expression, otherwise false.
-     */
-    private boolean checkIfValidBooleanExpression(String line) throws IllegalLineException {
-        Matcher ifMatcher = Regex.IF_PATTERN.matcher(line);
-        Matcher whileMatcher = Regex.WHILE_PATTERN.matcher(line);
-        String booleanExpression = null;
-        int expressionStart = 0;
-        int expressionEnd = 0;
-        if (ifMatcher.matches() || whileMatcher.matches()) {
-            expressionStart = line.indexOf('(');
-            expressionEnd = line.indexOf(')');
-            booleanExpression = line.substring(expressionStart, expressionEnd);
-            String[] splitExpression = booleanExpression.split(Regex.BOOLEAN_EXPRESSION_SPLIT);
-            for (String exp : splitExpression) {
-                Matcher doubleMatch = Regex.DOUBLE_PATTERN.matcher(exp);
-                if (!(exp.equals("true") || exp.equals("false") || doubleMatch.matches() ||
-                        checkIfInitializedVariable(exp))) {
-                    throw new IllegalLineException("error in line " + runner.GetLineNumber() + ", boolean " +
-                            "expression not supported.");
-                }
-            }
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * Checks if a variable is in the list, if it is return the varible object if not returns null.
+	 *
+	 * @param name The name of the variable to check if is in the list.
+	 * @return The variable object if it's in the list, otherwise null.
+	 */
+	private VariableWrapper getVariableFromList(String name) {
+		for (VariableWrapper var : variables) {
+			if (var.getName().equals(name)) {
+				return var;
+			}
+		}
+		return null;
+	}
 
-    /**
-     * Checks if a variable is in the list, if it is return the varible object if not returns null.
-     * @param name The name of the variable to check if is in the list.
-     * @return The variable object if it's in the list, otherwise null.
-     */
-    private VariableWrapper getVariableFromList(String name) {
-        for (VariableWrapper var : variables) {
-            if (var.getName().equals(name)) {
-                return var;
-            }
-        }
-        return null;
-    }
+	/**
+	 * Checks if a variable is an initialized variable.
+	 * !! can't check if uninitialized variable.
+	 *
+	 * @param name The name of the variable to check.
+	 * @return True if it's initialized, false else.
+	 */
+	protected boolean checkIfInitializedVariable(String name) {
+		VariableWrapper var = getVariableFromList(name);
+		if (var == null) {//not even in the list.
+			return false;
+		}
+		return var.getHasValue();
+	}
 
-    /**
-     * Checks if a variable is an initialized variable.
-     * !! can't check if uninitialized variable.
-     * @param name The name of the variable to check.
-     * @return True if it's initialized, false else.
-     */
-    protected boolean checkIfInitializedVariable(String name) {
-        VariableWrapper var = getVariableFromList(name);
-        if (var == null) {//not even in the list.
-            return false;
-        }
-        return var.getHasValue();
-    }
+	/**
+	 * Checks if a variable is an uninitialized variable.
+	 * !! can't check if initialized variable.
+	 *
+	 * @param name The name of the variable to check.
+	 * @return True if it's uninitialized, false else.
+	 */
+	protected boolean checkIfUninitializedVariable(String name) {
+		VariableWrapper var = getVariableFromList(name);
+		if (var == null) {//not even in the list.
+			return false;
+		}
+		return !var.getHasValue();
+	}
 
-    /**
-     * Checks if a variable is an uninitialized variable.
-     * !! can't check if initialized variable.
-     * @param name The name of the variable to check.
-     * @return True if it's uninitialized, false else.
-     */
-    protected boolean checkIfUninitializedVariable(String name) {
-        VariableWrapper var = getVariableFromList(name);
-        if (var == null) {//not even in the list.
-            return false;
-        }
-        return !var.getHasValue();
-    }
+	private boolean isFuncCallLegal(String line) {
+		Matcher m = Regex.funcNamePattern.matcher(line);
+		if (m.find()) {
+			String name = line.substring(m.start(), m.end());
+			boolean legalFunc = false;
+			for (FunctionWrapper func : master.getFuncs()) { // check if the function being called name
+				// is a name of a known function.
+				if (name.equals(func.getName())) {
+					legalFunc = true;
+					break;
+				}
+			}
+			if (!legalFunc) {
+				return false;
+			}
+		}
+	}
 }
--- src/oop/ex6/main/Blocks/MasterBlock.java
+++ src/oop/ex6/main/Blocks/MasterBlock.java
@@ -1,14 +1,150 @@
 package oop.ex6.main.Blocks;
 
+import oop.ex6.main.FunctionWrapper;
+import oop.ex6.main.IllegalLineException;
+import oop.ex6.main.Regex;
+import oop.ex6.main.VariableWrapper;
+
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+
 public class MasterBlock extends CodeBlock {
+
+	private static final String ERROR_START = "Error in line ";
+	private static final String VAR_ERROR = " variable deceleration is done poorly.";
+	private static final String FUNC_DEC_ERROR = " function deceleration is done poorly.";
+
+	private ArrayList<FunctionWrapper> funcs;
+
 	private static MasterBlock ourInstance = new MasterBlock();
+	private String[] lines;
 
 	public static MasterBlock getInstance() {
 		return ourInstance;
 	}
 
-	private MasterBlock() {
+	private MasterBlock(){
+		funcs = new ArrayList<FunctionWrapper>();
+		variables = new ArrayList<VariableWrapper>();
+		runner = new LinesRunner(lines);
+
+	}
+
+	public ArrayList<FunctionWrapper> getFuncs() {
+		return funcs;
+	}
+
+	/**
+	 * sets the lines if the master block.
+	 * @param lines array of lines.
+	 */
+	public void setLines(String[] lines){
+		this.lines =  lines;
+		runner.setLines(lines);
 	}
 
 	//TODO
+
+	/*
+	initializes lists of all the global methods and variables
+	 */
+	public void getGlobalDataMembers()
+			throws IllegalLineException {
+		int openBraces = 0;
+		String line = runner.GetNextLine();
+		while (line != null) {
+
+			if (openBraces == 0) {
+				checkGlobalLine(line);
+			}
+			if (line.endsWith("{")) {
+				openBraces += 1;
+			} else if (line.endsWith("}")) {
+				openBraces -= 1;
+			}
+			line = runner.GetNextLine();
+		}
+		System.out.println(this.funcs);
+	}
+
+	/*
+	checks a line for global variables and methods
+	 */
+	private void checkGlobalLine(String line) throws IllegalLineException {
+
+		Matcher funcLineStart = Regex.funcLineStartPattern.matcher(line);
+		Matcher varLineStart = Regex.varLinePattern.matcher(line);
+		if (funcLineStart.lookingAt()) {
+			this.funcs.add(lineToFuncObj(line));
+		} else if (varLineStart.lookingAt()) {
+			this.variables.add(lineToVarObj(line));
+		}
+	}
+
+	/*
+	gets a line of a variable deceleration, and returns a variable object. //TODO maybe dvir already did this.
+	 */
+	private static VariableWrapper lineToVarObj(String line) throws IllegalLineException {
+		Matcher varTemplateM = Regex.VARIABLE_TEMPLATE.matcher(line);
+		if (!varTemplateM.matches()) {
+			throw new IllegalLineException(ERROR_START + runner.GetLineNumber() + VAR_ERROR);
+		}
+		String[] temp = line.split("\\s");
+		return null;
+	}
+
+	/*
+	gets a line of a function deceleration, and returns a function wrapper object.
+	 */
+	private static FunctionWrapper lineToFuncObj(String line) throws IllegalLineException {
+		Matcher format = Regex.FUNCTION_TEMPLATE.matcher(line);
+		if (!format.matches()){
+			throw new IllegalLineException(ERROR_START + runner.GetLineNumber() + FUNC_DEC_ERROR); //TODO add excewption massege with line.
+		}
+		Matcher typesMatcher = Regex.typePattern.matcher(line);
+		ArrayList<VariableWrapper> params = new ArrayList<VariableWrapper>();
+		Matcher paramName = Regex.varNamePattern.matcher(line);
+		Matcher bracketsM = Regex.bracketsPattern.matcher(line);
+		boolean j = bracketsM.find(); // always true
+		String brackets = line.substring(bracketsM.start(),
+				bracketsM.end());
+		if (brackets.matches(".*\\s*,\\s*\\)\\s*")) {
+			throw new IllegalLineException(ERROR_START + runner.GetLineNumber() + FUNC_DEC_ERROR);
+		}
+		String[] typesAndVals = brackets.split(",");
+		int i = 0;
+		while (typesMatcher.find()) {
+			i = initParams(line, typesMatcher, params, typesAndVals[i], i);
+		}
+		Matcher name = Regex.funcNamePattern.matcher(line);
+		name.find();
+		name.find(); //twice to skip "void".
+		String funcName = line.substring(name.start(), name.end());
+		return new FunctionWrapper(params, funcName);
+	}
+
+	private static int initParams(String line, Matcher typesMatcher, ArrayList<VariableWrapper> params, String typesAndVal, int i) {
+		String[] curValAndType = typesAndVal.split("\\s+");
+		String curParamName;
+		if (curValAndType[0].equals("(")) {
+			curParamName = getVarName(curValAndType[2]);
+		} else {
+			curParamName = getVarName(curValAndType[1]);
+		}
+		int start = typesMatcher.start(), end = typesMatcher.end();
+		params.add(new VariableWrapper(line.substring(start, end), true, curParamName));
+		i++;
+		return i;
+	}
+
+	private static String getVarName(String s) {
+		String curParamName;
+		if (s.endsWith(")")) {
+			curParamName = s.substring(0, s.length() - 1);
+		} else {
+			curParamName = s;
+		}
+		return curParamName;
+	}
 }
+

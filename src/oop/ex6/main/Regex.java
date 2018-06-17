93bb3074d05f09688ba65c6503567bb7b230189c
tomerpeled1 <tomer.peled1@mail.huji.ac.il>
2018-06-17 21:02:37 +0300

    change [ ]* to \\s*

--- src/oop/ex6/main/Regex.java
+++ src/oop/ex6/main/Regex.java
@@ -12,24 +12,24 @@ public class Regex {
 
 	private static final String varName = "[a-zA-Z_][a-zA-z0-9_]*";
 
-	public static final Pattern FUNCTION_TEMPLATE = Pattern.compile("void[ ]+" + methodName + "[ ]*\\([ ]*([ ]*"
-			+ type + "[ ]+" + varName + "[ ]*,)*[ ]*" + type + "[ ]+" + varName + "[ ]*\\)\\{");
+	public static final Pattern FUNCTION_TEMPLATE = Pattern.compile("void\\s+" + methodName + "\\s*\\(\\s*(\\s*"
+			+ type + "\\s+" + varName + "\\s*,)*\\s*" + type + "\\s+" + varName + "\\s*\\)\\{\\s*");
 
 
-	public static final Pattern IF_PATTERN = Pattern.compile("[ ]*if[ ]*\\(.+\\)[ ]*\\{");
-	public static final Pattern WHILE_PATTERN = Pattern.compile("[ ]*while[ ]*\\(.+\\)[ ]*\\{");
+	public static final Pattern IF_PATTERN = Pattern.compile("\\s*if\\s*\\(.+\\)\\s*\\{");
+	public static final Pattern WHILE_PATTERN = Pattern.compile("\\s*while\\s*\\(.+\\)\\s*\\{");
 
 	public static final Pattern typePattern = Pattern.compile(type);
 	public static final Pattern varNamePattern = Pattern.compile(varName);
 	public static final Pattern bracketsPattern = Pattern.compile("\\(.*\\)");
-	public static final Pattern funcLineStartPattern = Pattern.compile("[ ]*void");
-	public static final Pattern varLineStartPattern = Pattern.compile("[ ]*" + type);
+	public static final Pattern funcLineStartPattern = Pattern.compile("\\s*void");
+	public static final Pattern varLineStartPattern = Pattern.compile("\\s*" + type);
 
 	public static final Pattern funcNamePattern = Pattern.compile(methodName);
 
 
-	public static final Pattern BLOCK_END_PATTERN = Pattern.compile("[ ]*}[ ]*");
+	public static final Pattern BLOCK_END_PATTERN = Pattern.compile("\\s*}\\s*");
 
-	public static final String BOOLEAN_EXPRESSION_SPLIT = "[ ]*(\\|\\||&&)[ ]*";
+	public static final String BOOLEAN_EXPRESSION_SPLIT = "\\s*(\\|\\||&&)\\s*";
 
 }

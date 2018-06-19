2c414dc4d21403ba6721c2c71b3f749f0eabbed5
tomerpeled1 <tomer.peled1@mail.huji.ac.il>
2018-06-20 00:23:15 +0300

    sabah el hir amir ho good morning regev

--- src/oop/ex6/main/Blocks/MasterBlock.java
+++ src/oop/ex6/main/Blocks/MasterBlock.java
@@ -10,6 +10,7 @@ import java.util.regex.Matcher;
 
 public class MasterBlock extends CodeBlock {
 
+
 	private static final String ERROR_START = "Error in line ";
 	private static final String VAR_ERROR = " variable deceleration is done poorly.";
 	private static final String FUNC_DEC_ERROR = " function deceleration is done poorly.";
@@ -23,7 +24,7 @@ public class MasterBlock extends CodeBlock {
 		return ourInstance;
 	}
 
-	private MasterBlock(){
+	private MasterBlock() {
 		super();
 		funcs = new ArrayList<FunctionWrapper>();
 		variables = new ArrayList<VariableWrapper>();
@@ -37,17 +38,19 @@ public class MasterBlock extends CodeBlock {
 
 	/**
 	 * sets the lines if the master block.
+	 *
 	 * @param lines array of lines.
 	 */
-	public void setLines(String[] lines){
-		this.lines =  lines;
-		runner.setLines(lines);
+	public void setLines(String[] lines) {
+		this.lines = lines;
+		runner = new LinesRunner(lines);
 	}
 
 	//TODO
 
 	/**
 	 * Initiliazes the the functions and global variables of the master block.
+	 *
 	 * @throws IllegalLineException If
 	 */
 	public void getGlobalDataMembers()
@@ -87,7 +90,7 @@ public class MasterBlock extends CodeBlock {
 	 */
 	private static FunctionWrapper lineToFuncObj(String line) throws IllegalLineException {
 		Matcher format = Regex.FUNCTION_TEMPLATE.matcher(line);
-		if (!format.matches()){
+		if (!format.matches()) {
 			throw new IllegalLineException(ERROR_START + runner.getLineNumber() + FUNC_DEC_ERROR);
 		}
 		Matcher typesMatcher = Regex.typePattern.matcher(line);
@@ -108,31 +111,32 @@ public class MasterBlock extends CodeBlock {
 		name.find(); //twice to skip "void".
 		String funcName = line.substring(name.start(), name.end());
 		if (funcName.equals("void")) {
-            throw new IllegalLineException("error in line " + runner.getLineNumber() +
-                    ", function can't be named void");
-        }
+			throw new IllegalLineException("error in line " + runner.getLineNumber() +
+					", function can't be named void");
+		}
 		return new FunctionWrapper(params, funcName);
 	}
 
 
 	/**
 	 * is used to initialize a VariableWrapper object from a code chunk, and add it to the list it gets.
+	 *
 	 * @param params the list of VariableWrapper objects we want to add the current parameter to.
-	 * @param s the string from which we want to create the Variable. will be in a format of a
-	 *             variable deceleration.
+	 * @param s      the string from which we want to create the Variable. will be in a format of a
+	 *               variable deceleration.
 	 */
 	private static void initParam(ArrayList<VariableWrapper> params, String s) {
 
 		String[] temp = s.split("\\s+");
 		for (int j = 0; j < temp.length; j++) {
-			temp[j] = temp[j].replaceAll("\\(","");
+			temp[j] = temp[j].replaceAll("\\(", "");
 		}
-		switch (temp.length){
+		switch (temp.length) {
 			case 2:
-				params.add(new VariableWrapper(temp[0],true,temp[1],false));
+				params.add(new VariableWrapper(temp[0], true, temp[1], false));
 				break;
 			case 3:
-				params.add(new VariableWrapper(temp[1],true,temp[2],true));
+				params.add(new VariableWrapper(temp[1], true, temp[2], true));
 				break;
 		}
 
@@ -147,6 +151,28 @@ public class MasterBlock extends CodeBlock {
 //		params.add(new VariableWrapper(line.substring(start, end), true, curParamName));
 //		i++;
 //		return i;
+	}
+
+	@Override
+	public void run() throws IllegalLineException {
+		String line = runner.getNextLine();
+		while (line != null) {
+			if (line.startsWith("//")){
+				line = runner.getNextLine();
+				continue;
+			}
+			line = line.replaceFirst("\\s*", "");
+			String[] words = line.split("\\s+");
+			Matcher startWithType = Regex.typePattern.matcher(line);
+			if (startWithType.lookingAt()) {
+				continue;
+			} else if (getVariableIfExists(words[0]) != null) {
+
+			} else if (words[0].equals("void")) {
+
+			}
+		}
+		return;
 	}
 
 //	private static String getVarName(String s) { TODO clean here at the end if needed.
